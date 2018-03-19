414ebd80792a05f29ae50e3ed5fc7c071c536f61
Sergi Cuenca Luque <al315546@uji.es>
2018-03-19 19:52:42 +0100

    Vision primitiva y binding de tareas en el planificador. El siguiente paso es conectar con firebase y que tome de allí los datos.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,55 @@
 <template>
     <div id="dashboard">
-      <h3>Principal</h3>
-      <p>Esta es la página principal, aquí es donde, en un futuro, estará el planificador</p>
+
+      <div class="wn-col col-pendientes">
+        <div class="wn-col-title">Tareas Pendientes <button class="wn-menu-btn">Nueva tarea</button></div>
+
+        <div v-for="task in tasks_planificadas" class="wn-task-container">
+          <div class="wn-task-data">
+            <h3>{{task.title}}</h3>
+            <p>Oprerario:  {{task.operario}}</p>
+            <p>Duración: {{task.duracion}}/{{task.estimado}} min</p>
+          </div>
+          <div class="wn-btn-div">
+            <button class="wn-menu-btn">Eliminar</button>
+            <button class="wn-menu-btn disabled">Prioridad</button>
+            <button class="wn-menu-btn">Editar</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="wn-col col-asignadas">
+        <div class="wn-col-title">Tareas Asignadas</div>
+        <div v-for="task in tasks_asignadas" class="wn-task-container">
+          <div class="wn-task-data">
+            <h3>{{task.title}}</h3>
+            <p>Oprerario:  {{task.operario}}</p>
+            <p>Duración: {{task.duracion}}/{{task.estimado}} min</p>
+          </div>
+          <div class="wn-btn-div">
+            <button class="wn-menu-btn">Eliminar</button>
+            <button class="wn-menu-btn disabled">Prioridad</button>
+            <button class="wn-menu-btn">Editar</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="wn-col col-realizadas">
+        <div class="wn-col-title ">Tareas Realizadas</div>
+        <div v-for="task in tasks_realizadas" class="wn-task-container">
+          <div class="wn-task-data">
+            <h3>{{task.title}}</h3>
+            <p>Oprerario:  {{task.operario}}</p>
+            <p>Duración: {{task.duracion}}/{{task.estimado}} min</p>
+          </div>
+          <div class="wn-btn-div">
+            <button class="wn-menu-btn">Eliminar</button>
+            <button class="wn-menu-btn disabled">Prioridad</button>
+            <button class="wn-menu-btn">Editar</button>
+          </div>
+        </div>
+      </div>
+
     </div>
 </template>
 
@@ -10,8 +58,104 @@
     name: 'dashboard',
     data() {
       return {
+        tasks_planificadas: [
+          { title: 'Reparar grifo',     operario:'Pablito Perez', duracion:0, estimado:10 },
+          { title: 'Reparar ventana',   operario:'Juan Lopez',    duracion:0, estimado:12 },
+          { title: 'Comprar bombillas', operario:'Juan Lopez',    duracion:0, estimado:20 }
+        ],
 
+        tasks_asignadas: [
+          { title: 'Cambiar bombilla',     operario:'Pablito Perez', duracion:9, estimado:10 },
+          { title: 'Desmontar ventana',    operario:'Juan Lopez',    duracion:23, estimado:20 }
+        ],
+
+        tasks_realizadas: [
+          { title: 'Reparar grifo',     operario:'Carlos Martinez', duracion:12, estimado:10 }
+        ]
       }
     }
   }
 </script>
+
+<style>
+  div.wn-col div{
+    padding-left:  10px;
+    padding-top:    3px;
+    padding-bottom: 3px;
+  }
+  div.wn-col {
+    width: 33.3333%;
+    float: left;
+    margin-top: -9px;
+    border: solid 2px white;
+    height: auto;
+  }
+  div.wn-col-title {
+    background: cadetblue;
+    color: white;
+    font-size: 16px;
+    padding-bottom: 2px;
+  }
+  div.wn-task-container {
+    margin-top: 3px;
+    background: whitesmoke;
+    height: 80px;
+  }
+  div.col-pendientes {
+    border-left: none;
+  }
+  div.col-asignadas {
+    border-left: none;
+    border-right: none;
+  }
+  div.col-realizadas {
+    border-right: none;
+  }
+
+
+  div.wn-task-data {
+    width: 80%;
+    float: left;
+  }
+
+  div.wn-task-data h3 {
+    font-size:     16px;
+  }
+
+  div.wn-task-data p {
+    font-size:     12px;
+    padding-top:    4px;
+    padding-bottom: 0px;
+    margin-bottom:  -6px;
+  }
+
+  div.wn-btn-div {
+    width: 2px;
+    float: right;
+  }
+
+  button.wn-menu-btn {
+    border: none;
+    background: whitesmoke;
+    color: cadetblue;
+    font-size: 12px;
+    float: right;
+    width: 140px;
+    padding-top:    2px;
+    padding-bottom: 2px;
+    padding-left:  34px;
+    padding-right: 34px;
+    margin-top:     1px;
+    margin-right:  24px;
+  }
+
+  div.wn-btn-div .wn-menu-btn {
+    width: 100px;
+    font-size: 12px;
+    text-align: left;
+    float: right;
+    margin-top:    0px;
+    margin-bottom: 0px;
+  }
+
+</style>
