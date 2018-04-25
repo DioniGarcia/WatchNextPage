17d5990bc52f7e5ac20c47e78edea16dd29f1861
Sergi Cuenca Luque <al315546@uji.es>
2018-04-25 23:44:39 +0200

    Función "solicitarDatosTarea" completada y devolviendo un JSON

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,46 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+
+exports.solicitarDatosTarea = functions.https.onRequest((req, res) => {
+
+  var idOprario = req.query.id;
+  var tareas = [];
+
+  return admin.firestore().collection('operariosConectados').doc(idOprario.toString()).get()
+    .then(doc => {
+      tareas=doc.data().tareas
+
+      if (tareas.length == 0)
+        res.status(404).send("No hay tareas");
+      else
+
+        return admin.firestore().collection('sinAsignar').doc(tareas[0]).get()
+          .then(doc => {
+            var titulo = doc.data().titulo
+            var descripcion = doc.data().descripcion
+            var estimado=  doc.data().estimado
+            var prioridad=  doc.data().prioridad
+
+            res.status(200).json("{ "+
+              "titulo: '"+titulo+"', "+
+              "descripcion: '"+descripcion+"', "+
+              "estimado: '"+estimado+"', "+
+              "prioridad: '"+prioridad+"'"
+              +" }"
+            );
+          });
+    });
+  });
+
+
+
+  //operariosConectados.doc(idOprario).get(tareas)
+  //if tareas.lenght = 0
+  //   resp "No hay tareas"
+  //else
+  //   resp "tarea.json"
+
 exports.saludar = functions.https.onRequest((req, res) => {
 
   var nombre = req.query.nombre;
